e4370fe644163736b66d09bcfe83bbfbc6b41794
mrroberts-mslt <82210970+mrroberts-mslt@users.noreply.github.com>
2021-04-09 11:58:14 +0100

    Create session01c.py

---
+++ session01c.py
@@ -0,0 +1,5 @@
+print("Area of a rectangle:", 40*10)
+print("My age in 2059: ", (2059-2021)+45)
+print("My Shoes cost 85 and I paid 21% tax:", 85-(85/1.21))
+print("To travel to dublin takes", 232/60)
+print("The speed of light is", 149840000/500,"km/s")
